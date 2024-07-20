c55b52a939420868e02d89f51eef722e5f323bfe
Mr-Muneeb101 <160252527+Mr-Muneeb101@users.noreply.github.com>
2024-07-20 12:05:32 -0700

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import { isNumberObject } from "util/types";
+class Student {
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+class Person {
+    students: Student[] = [];
+    addStudent(name: Student) {
+        this.students.push(name)
+    }
+}
+let person = new Person;
+const main = async (person1: Person) => {
+    do {
+        let Userinput = await inquirer.prompt([
+            {
+                name: "option",
+                type: "list",
+                message: "Select Whom you wanna Talk",
+                choices: ["Staff", "Student", "Exit"]
+            }
+        ]);
+        switch (Userinput.option) {
+            case "Staff":
+                console.log(`Hello I am talking from staff,Hope You are having a great day?`);
+                break;
+            case "Student":
+                let Student_name = await inquirer.prompt([
+                    {
+                        name: "S_name",
+                        type: "input",
+                        message: "please Enter the name of Student",
+                    }
+                ]);
+
+
+                let findStudent = person1.students.find((student) => student.name === Student_name.S_name);
+                if (findStudent) {
+                    console.log(`Hello I am ${findStudent.name}, Are you all Okay!?`);
+                } else if (!findStudent) {
+                    console.log(`Currrent Student List: ${person1.students}`);
+                    console.log(`\n\t\t New Student Added \n`);
+                    let newStudent = new Student(Student_name.S_name);
+                    console.log(`Hello I am ${newStudent.name}, , Are you all Okay!?`);
+                    person1.addStudent(Student_name.S_name);
+                }
+                break;
+            case "Exit":
+                console.log(`Good Bye`);
+                return false;
+
+        }
+    } while (true);
+}
+main(person)
